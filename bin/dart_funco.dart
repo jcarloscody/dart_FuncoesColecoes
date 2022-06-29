cd742313f946d69d7e6342a058035331ca4a89a6
Josué Carlos <josuecarlosos2@gmail.com>
2022-06-29 03:32:22 -0300

    starting

---
+++ bin/dart_funco.dart
@@ -0,0 +1,5 @@
+import 'package:dart_funco/dart_funco.dart' as dart_funco;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_funco.calculate()}!');
+}
---
+++ test/dart_funco_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_funco/dart_funco.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
